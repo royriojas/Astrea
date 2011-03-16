b90efd959d8cfdc09a4d7ea7c47a1d06d75d480c
Roy Riojas <roy@kno.com>
2011-03-16 10:42:25 -0700

    Added Specit a Library for BDD

--- astrea/b.js
+++ astrea/b.js
@@ -1,6 +1,32 @@
 /**
  * @author Roy Riojas
  */
-(function () {
-	
+(function ($) {
+
+  var Astrea = window.$a || {};
+
+  if (Astrea.loaded)
+    return; //just avoid to run this function again if already loaded astrea
+
+  $.extend(Astrea, {
+    /**
+     * return a string formatted with the passed arguments
+     * @example
+     *
+     * var s = $a.format('Some {0} with a {1} mood {2}', 'String', 'good', 'arrived');
+     * //output
+     * //s = 'Some String with a good mood arrived'
+     */
+    format : function () {
+      var pattern = /\{\d+\}/g;
+      var args = arguments;
+      var s = Array.prototype.shift.apply(args);
+      return s.replace(pattern, function(c) {
+        return args[c.match(/\d+/)];
+      });
+    }
+  });
+
+  Astrea.loaded = true;
+  window.$a = Astrea;
 })(jQuery);
